f1d6c2a2082c66f89dfa7a48f7038173b984fc10
Ane <anemcleod@gmail.com>
2020-11-30 12:28:10 +0530

    completed all userRouter routes

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -8,6 +8,7 @@ const Job = require("../models/jobModel");
 
 
 
+
 const signToken = userID =>{
     return JWT.sign({
         iss : "Jobseeker",
@@ -77,9 +78,21 @@ userRouter.get('/logout',passport.authenticate('jwt',{session : false}),(req,res
     res.json({user:{username : ""},success : true});
 });
 
-userRouter.get('/test',passport.authenticate('jwt',{session : false}),(req,res)=>{
-    res.json({message: "works"});
-});
+userRouter.delete("/delete", passport.authenticate('jwt',{session : false}), async (req, res) => {
+    try {
+      const user = await User.findById(req.user);
+      const jobBoards = user.jobBoards;
+
+      for (board of jobBoards) {
+          await Job.deleteMany({ jobBoardId: board});
+          await JobBoard.findByIdAndDelete(board);
+      }
+      const deletedUser = await User.findByIdAndDelete(req.user);
+      res.json(deletedUser);
+    } catch (err) {
+      res.status(500).json({message : {msgBody: err.message, msgError: true}});
+    }
+  });
 
 
 
@@ -88,12 +101,12 @@ userRouter.get('/test',passport.authenticate('jwt',{session : false}),(req,res)=
 userRouter.post('/jobBoard',passport.authenticate('jwt',{session : false}), async (req,res)=>{
     try {
         const jobBoard = new JobBoard(req.body);
-        await jobBoard.save(err=>{
+        await jobBoard.save( async err=>{
             if(err)
                 res.status(500).json({message : {msgBody : "Error has occured", msgError: true}});
             else{
                 req.user.jobBoards.push(jobBoard);
-                req.user.save(err=>{
+                await req.user.save(err=>{
                     if(err)
                         res.status(500).json({message : {msgBody : "Error has occured", msgError: true}});
                     else
@@ -110,5 +123,75 @@ userRouter.post('/jobBoard',passport.authenticate('jwt',{session : false}), asyn
    
 });
 
+userRouter.delete("/jobBoard/:jobBoardId", passport.authenticate('jwt',{session : false}), async (req, res) => {
+    try {
+      const jobBoard = await JobBoard.findById(req.params.jobBoardId);
+      const jobs = jobBoard.jobs;
+
+      for(job of jobs){
+          await Job.findByIdAndDelete(job);
+      }
+
+      const deletedJobBoard = await JobBoard.findByIdAndDelete(req.params.jobBoardId);
+      const UpdatedUser = await User.updateOne({"_id" : req.user } ,{ "$pull": {  "jobBoards": req.params.jobBoardId } }, { multi: true });
+      res.json(deletedJobBoard).json(UpdatedUser);
+    } catch (err) {
+      res.status(500).json({message : {
+          msgBody: err.message, 
+          msgError: true}});
+    }
+  });
+
+// =========================== job routes ============================== //
+
+userRouter.post("/jobs", passport.authenticate('jwt',{session : false}), async (req, res) =>{
+    try{
+        const { jobBoardId, title, company, location, description, link, date, contract } = req.body;
+        const job = new Job({
+                                jobBoardId, 
+                                title, 
+                                company, 
+                                location, 
+                                description, 
+                                link, 
+                                date, 
+                                contract
+                            });
+        await job.save(async (err) => {
+            if(err){
+                res.status(500).json({message : {msgBody : "Error has occured", msgError: true}});
+            } else {
+                const jobBoard = await JobBoard.findById(job.jobBoardId);
+                jobBoard.jobs.push(job);
+                await jobBoard.save(err=>{
+                    if(err)
+                        res.status(500).json({message : {msgBody : "Error has occured", msgError: true}});
+                    else
+                        res.status(200).json({message : {msgBody : "Successfully created job", msgError : false}});
+                });
+            }
+        })
+
+    } catch (err) {
+        res.status(500).json({message: {
+            msgBody: err.message, 
+            msgError: true
+        }})
+    }
+});
+
+userRouter.delete("/jobs/:jobId", passport.authenticate('jwt',{session : false}), async (req, res) => {
+    try {
+        const job = await Job.findById(req.params.jobId);
+        const updatedJobBoard = await JobBoard.findOneAndUpdate({"_id": job.jobBoardId}, { "$pull": {  "jobs": req.params.jobId } }, { multi: true });
+        const deletedJob = await Job.findByIdAndDelete(req.params.jobId);
+        res.json(deletedJob).json(updatedJobBoard);
+    } catch (err) {
+        res.status(500).json({message: {
+            msgBody: err.message, 
+            msgError: true
+        }})
+    }   
+});
 
 module.exports = userRouter;
